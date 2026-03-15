da0acc3adf0a1edab07370380041f60299f30043
yvano123 <yvanovdwal@gmail.com>
2026-03-15 22:08:48 +0100

    Implemented new ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { airports } from '$lib/Constants/Airports';
 	import { onMount } from 'svelte';
 	import { isIfStatement } from 'typescript';
+	import Cross from '$lib/assets/cross.svg';
 
 	let as: HTMLAudioElement;
 	let ms: HTMLAudioElement;
@@ -12,6 +13,14 @@
 	let musicVolume = $state(0.1);
 	let playing = $state(false);
 	let connected = $state(0);
+
+	let crossThing = $state(Cross);
+	const CrossIcon = $derived(crossThing);
+
+	$effect(() => {
+		if (!selectOpen) searchTerm = '';
+	});
+
 	function checkStates() {
 		connected = as?.readyState;
 		playing = !as?.paused || !ms?.paused;
@@ -69,46 +78,18 @@
 <audio bind:volume bind:this={as} id="as" hidden> </audio>
 <div class="flex h-lvh items-center justify-center bg-olive-100">
 	<div class="flex aspect-square h-6/10 flex-col items-center rounded-2xl bg-olive-300 p-8">
-		<div class="flex flex-col items-center">
+		<div class="flex w-full flex-col items-center">
 			<!-- HEADER -->
 
-			<div class="flex w-full flex-row items-center justify-between">
-				<div class="flex flex-row items-center gap-0.5 text-transparent select-none">
-					<p class="text-3xl text-transparent">•</p>
-					<p class="text-xl">
-						{connected < 3 && selected == ''
-							? 'No connection'
-							: connected < 3
-								? 'Connecting'
-								: 'LIVE'}
-					</p>
-				</div>
+			<div class="flex w-full flex-row items-center justify-center">
 				<h1 class="text-4xl">LofiATC</h1>
-				<div class="flex flex-row items-center gap-0.5">
-					<p
-						class="text-3xl text-red-500"
-						class:text-red-500={connected >= 3}
-						class:text-yellow-500={connected < 3 && selected != ''}
-						class:text-gray-500={selected == ''}
-						class:animate-pulse={connected >= 3}
-					>
-						•
-					</p>
-					<p class="text-xl">
-						{connected < 3 && selected == ''
-							? 'No connection'
-							: connected < 3
-								? 'Connecting'
-								: 'LIVE'}
-					</p>
-				</div>
 			</div>
 			<!-- /HEADER -->
 
 			<!-- SELECTED STATION -->
-			<div class="mt-3 w-full rounded-xl bg-olive-400 p-3">
+			<div class="mt-3 w-9/10 rounded-xl bg-olive-400 p-3">
 				<div class="flex flex-row items-center justify-between gap-0.5">
-					<p></p>
+					<p class="pl-2">{airports.find((x) => x.code == selected)?.code ?? ''}</p>
 					<div class="flex flex-row items-center">
 						<p
 							class="text-2xl text-red-500"
@@ -119,11 +100,19 @@
 						>
 							•
 						</p>
-						<p class="">LIVE</p>
+						<p class="">
+							{connected < 3 && selected == ''
+								? 'No connection'
+								: connected < 3
+									? 'Connecting'
+									: 'LIVE'}
+						</p>
 					</div>
 				</div>
 				<div class="flex flex-row items-center justify-between">
-					<p>No Station Selected</p>
+					<p class="max-w-8/10 rounded-xl bg-olive-300 p-2">
+						{airports.find((x) => x.code == selected)?.name ?? 'No airport selected'}
+					</p>
 					<button
 						onclick={() => {
 							selectOpen = true;
@@ -220,10 +209,11 @@
 				onclick={() => {
 					selectOpen = false;
 				}}
-				class="absolute flex">Back</button
+				class="absolute flex transition-transform duration-700 hover:rotate-360 hover:cursor-pointer"
+				><img class="aspect-square w-10" src={Cross} alt="logo" /></button
 			>
+			<div class="flex h-full w-full items-center justify-center"><p>SELECT AIRPORT</p></div>
 		</div>
-		<div class="flex h-full w-full items-center justify-center"><p>SELECT A STATION</p></div>
 	</div>
 	<!-- SEARCH BAR -->
 	<div class="flex flex-col items-center justify-center gap-2">
@@ -238,9 +228,44 @@
 		{#if !searchTerm}
 			<p>or</p>
 			<button
+				onclick={() => {
+					let airport = airports[Math.floor(Math.random() * airports.length)];
+					if (as.paused) {
+						as.src = airport.uri;
+						selected = airport.code;
+					} else {
+						as.src = airport.uri;
+						selected = airport.code;
+						as.play();
+					}
+					selectOpen = false;
+				}}
 				class="h-20 w-2/10 rounded-2xl bg-olive-400 px-1 text-2xl transition-transform hover:scale-105 hover:cursor-pointer"
 				>Random airport</button
 			>
+		{:else}
+			<div class="flex h-3/10 w-3/10 flex-col gap-1.5 p-4">
+				{#each airports.filter((x) => x.code.includes(searchTerm.toUpperCase())) as airport}
+					<button
+						onclick={() => {
+							if (as.paused) {
+								as.src = airport.uri;
+								selected = airport.code;
+							} else {
+								as.src = airport.uri;
+								selected = airport.code;
+								as.play();
+							}
+							selectOpen = false;
+						}}
+						class:border-2={selected == airport?.code}
+						class="flex w-full flex-col justify-baseline rounded-2xl bg-olive-400 p-2 transition-transform hover:scale-105 hover:cursor-pointer"
+					>
+						<p class="self-start">{airport.code}</p>
+						<p class="self-start">{airport.name}</p>
+					</button>
+				{/each}
+			</div>
 		{/if}
 	</div>
 </div>
