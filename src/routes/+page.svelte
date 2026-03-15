3788d9178052b27618e4754e1a0bbaa8e5631482
yvano123 <yvanovdwal@gmail.com>
2026-03-15 18:53:23 +0100

    wip new ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
 
 	let as: HTMLAudioElement;
 	let ms: HTMLAudioElement;
+	let searchTerm = $state('');
+	let selectOpen = $state(false);
 	let selected = $state('');
 	let volume = $state(0.5);
 	let musicVolume = $state(0.1);
@@ -65,11 +67,11 @@
 	src="https://live.hunter.fm/lofi_high"
 ></audio>
 <audio bind:volume bind:this={as} id="as" hidden> </audio>
-<div class="flex h-lvh items-center justify-center bg-olive-100">
-	<div
-		class="flex aspect-square flex-col items-center justify-between rounded-2xl bg-olive-300 p-8"
-	>
+<div class="relative flex h-lvh items-center justify-center bg-olive-100">
+	<div class="z-20 flex aspect-square h-6/10 flex-col items-center rounded-2xl bg-olive-300 p-8">
 		<div class="flex flex-col items-center">
+			<!-- HEADER -->
+
 			<div class="flex w-full flex-row items-center justify-between">
 				<div class="flex flex-row items-center gap-0.5 text-transparent select-none">
 					<p class="text-3xl text-transparent">•</p>
@@ -101,26 +103,39 @@
 					</p>
 				</div>
 			</div>
+			<!-- /HEADER -->
 
-			<div class="my-4 grid grid-cols-6 flex-wrap gap-2">
-				{#each airports as airport}
+			<!-- SELECTED STATION -->
+			<div class="mt-3 w-full rounded-xl bg-olive-400 p-3">
+				<div class="flex flex-row items-center justify-between gap-0.5">
+					<p></p>
+					<div class="flex flex-row items-center">
+						<p
+							class="text-2xl text-red-500"
+							class:text-red-500={connected >= 3}
+							class:text-yellow-500={connected < 3 && selected != ''}
+							class:text-gray-500={selected == ''}
+							class:animate-pulse={connected >= 3}
+						>
+							•
+						</p>
+						<p class="">LIVE</p>
+					</div>
+				</div>
+				<div class="flex flex-row items-center justify-between">
+					<p>No Station Selected</p>
 					<button
 						onclick={() => {
-							if (as.paused) {
-								as.src = airport.uri;
-								selected = airport.code;
-							} else {
-								as.src = airport.uri;
-								selected = airport.code;
-								as.play();
-							}
+							selectOpen = true;
 						}}
-						class:border-2={selected == airport?.code}
-						class="aspect-square rounded-2xl bg-olive-400 p-2 transition-transform hover:scale-105 hover:cursor-pointer"
-						>{airport.code}</button
+						class="rounded-xl bg-olive-300 p-1 transition-transform hover:scale-105 hover:cursor-pointer"
+						>Select</button
 					>
-				{/each}
+				</div>
 			</div>
+			<!-- /SELECTED STATION -->
+
+			<div class="my-4 grid grid-cols-6 flex-wrap gap-2"></div>
 			<div class="flex flex-row gap-1">
 				<button
 					onclick={() => {
@@ -189,6 +204,43 @@
 				}}
 				class="text-xs text-olive-600 hover:cursor-pointer hover:underline">Reload the page</button
 			>
+		</div>
+	</div>
+	<div
+		class="z-10 flex h-6/10 w-2/10 flex-col justify-end gap-1 rounded-2xl bg-olive-400 p-5 transition-all {selectOpen
+			? ''
+			: '-ml-100'}"
+	>
+		<div class="flex flex-row gap-1">
+			<input bind:value={searchTerm} class="bg-olive-300" type="text" />
+			<button
+				class=""
+				onclick={() => {
+					selectOpen = false;
+				}}>X</button
+			>
+		</div>
+		<div class=" flex h-9/10 flex-col gap-0.5 overflow-y-scroll">
+			{#each airports.filter((x) => x.name.toLowerCase().includes(searchTerm) || x.code
+						.toLowerCase()
+						.includes(searchTerm)) as airport}
+				<button
+					onclick={() => {
+						if (as.paused) {
+							as.src = airport.uri;
+							selected = airport.code;
+						} else {
+							as.src = airport.uri;
+							selected = airport.code;
+							as.play();
+						}
+					}}
+					class:border-2={selected == airport?.code}
+					class="rounded- w-9/10 bg-olive-300 p-2 transition-transform hover:scale-105 hover:cursor-pointer"
+					><p>{airport.code}</p>
+					<p>{airport.name}</p></button
+				>
+			{/each}
 		</div>
 	</div>
 </div>
